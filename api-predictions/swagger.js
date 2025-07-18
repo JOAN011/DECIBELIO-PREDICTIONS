2bfe8dde99c140e708eb60d7c072fd0eb4534421
joan <blackcatsstart@gmail.com>
2025-07-18 05:52:39 -0500

    IP update

--- api-predictions/swagger.js
+++ api-predictions/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000', // Ajusta al puerto que uses
+        url: 'http://40.76.118.59:3000', // Ajusta al puerto que uses
       },
     ],
   },
